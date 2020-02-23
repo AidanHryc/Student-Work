66ab61c857db78a55e56b6ee56601fe3ef5830b4
Aidan Hryc <61389947+AidanHryc@users.noreply.github.com>
2020-02-23 17:44:34 -0500

    Update test.java

--- Lab6 - Trees/test.java
+++ Lab6 - Trees/test.java
@@ -1,12 +1,5 @@
 public class test{
 	public static void main(String...args){
-	/*
-		Integer ye[] = new Integer[10];
-		ye[0] = 2;
-		Integer z = ye[1];
-		Integer fall = ye[0];
-		System.out.println(fall<z);
-	*/
 		int x = 6;
 		System.out.println((int)(Math.log(x)/Math.log(2)));
 	}
